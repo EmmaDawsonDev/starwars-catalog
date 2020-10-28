44b39591ef04bce885f5ff8a7785ce7098e38404
PelleLindroth <pellelindroth@gmail.com>
2020-10-28 15:52:47 +0100

    fetch api logic added

--- js/main.js
+++ js/main.js
@@ -1,7 +1,29 @@
+const characterList = document.querySelector('.character-list')
+const btnLeft = document.querySelector('.left-btn')
+const btnRight = document.querySelector('.right-btn')
+
 async function getCharacters() {
   let data = await fetch('https://swapi.dev/api/people/')
   let characters = await data.json()
-  console.log(characters.results[0].homeworld)
+  console.log(characters)
+  return characters
 }
 
-getCharacters()
+async function renderCharList() {
+  let data = await getCharacters()
+  let characters = data.results
+
+  for (let char of characters) {
+    let li = document.createElement('li')
+    li.innerText = char.name
+    characterList.appendChild(li)
+  }
+}
+
+renderCharList()
+
+btnRight.addEventListener('click', async () => {
+  let characters = await getCharacters()
+  let next10 = characters.next
+  console.log(next10)
+})
